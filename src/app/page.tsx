02275eee52cf54469124835c3ab3aa97df8b6047
Amit (work computer) <kedemami2@gmail.com>
2026-04-12 17:55:18 +0300

    remove useless page and leave only home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import Link from "next/link";
-import "./page.css";
+import { permanentRedirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <div className="root-page layout-grid">
-      <main className="root-page-main">
-        <h1>Portfolio</h1>
-        <p>
-          View the portfolio at{" "}
-          <Link href="/portfolio/home">/portfolio/home</Link>.
-        </p>
-      </main>
-    </div>
-  );
+export default function RootPage() {
+  permanentRedirect("/portfolio/home");
 }
